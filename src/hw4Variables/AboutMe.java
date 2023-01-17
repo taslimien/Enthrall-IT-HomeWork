d4d7b64b4fa1152553c925c4d878f3bc6c2db2bc
Mohammad Uddin <mtaslimu010@gmail.com>
2023-01-17 00:41:44 -0500

    update

---
+++ src/hw4Variables/AboutMe.java
@@ -0,0 +1,31 @@
+package hw4Variables;
+
+public class AboutMe {
+
+	// Here variable declared.
+	public String name;
+	public byte age;
+	public short myCarPayment;
+	public int myYearlyHouseRent;
+	public int myHouseNumber;
+	public long ourBankBalance;
+	public float myWeight;
+	public double myGrade;
+	public char mySex;
+	public boolean myUsGreenCard;
+
+	// Here Constructor declared.
+	public AboutMe() {
+		System.out.println("This is all about me: ");
+
+	}
+
+	// Here method implemented.
+	public void aboutMe() {
+		System.out.println("My name: " + name + "\nMy age: " + age + "\nMy monthely car payment: " + myCarPayment
+				+ "\nMy yearly house rent: " + myYearlyHouseRent + "\nMy house number: " + myHouseNumber
+				+ "\nOur bank balance: " + ourBankBalance + "\nMy weight: " + myWeight + "\nMy grade: " + myGrade
+				+ "\nMy sex: " + mySex + "\nMy US green card: " + myUsGreenCard);
+	}
+
+}
---
+++ src/hw4Variables/AboutMeTest.java
@@ -0,0 +1,41 @@
+package hw4Variables;
+
+public class AboutMeTest {
+
+public static void main(String[] args) {
+		
+		// Here constructor is initialized.
+		AboutMe taslim = new AboutMe();
+		
+		// Here variables are initialized.
+		taslim.name ="Mohammad Taslim Uddin.";
+		taslim.age = 38;
+		taslim.myCarPayment = 600;
+		taslim.myYearlyHouseRent = 36000;
+		taslim.myHouseNumber = 349;
+		taslim.ourBankBalance = 9223372036854775807l;
+		taslim.myWeight = 183.1415927f;
+		taslim.myGrade = 3.1415927410125732;
+		taslim.mySex = 'M';
+		taslim.myUsGreenCard = false;
+		
+		//Here method is initialized.
+		taslim.aboutMe();
+		
+		System.out.println("\n------------------------------------------------------------\n");
+		
+		AboutMe alex =new AboutMe();
+		alex.name = "Alex";
+		alex.age = 40;
+		alex.myCarPayment = 500;
+		alex.myYearlyHouseRent = 35000;
+		alex.myHouseNumber = 607;
+		alex.ourBankBalance = 8223372036854775807l;
+		alex.myWeight = 193.1415927f;
+		alex.myGrade = 2.9415927410125732;
+		alex.mySex = 'M';
+		alex.myUsGreenCard = true;
+		alex.aboutMe();
+
+	}
+}
